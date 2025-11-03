4a5c10381c9bc795e218fced904d89ecd689b953
GrayMouse9 <zenacederbaum@gmail.com>
2025-11-03 13:59:52 +0300

    Выполнено РК 1 (Вариант 17, Дирижер/Оркестр)

--- rk1/main.py
+++ rk1/main.py
@@ -16,7 +16,7 @@ class Orchestra:
 
 class ConductorOrchestra:
     """
-    'Дирижеры оркестра' для реализации связи многие-ко-многим [cite: 13]
+    'Дирижеры оркестра' для реализации связи многие-ко-многим
     """
     def __init__(self, conductor_id, orchestra_id):
         self.conductor_id = conductor_id
@@ -59,7 +59,7 @@ conductors_orchestras = [
 def main():
     """Основная функция"""
 
-    # --- Подготовка данных ---
+    #Подготовка данных
 
     # Соединение данных один-ко-многим (Дирижер "принадлежит" одному Оркестру)
     # (Фамилия дирижера, Зарплата дирижера, Название оркестра)
@@ -69,19 +69,16 @@ def main():
                    if c.orchestra_id == o.id]
 
     # Соединение данных многие-ко-многим
-    # 1. Промежуточное соединение
     many_to_many_temp = [(o.name, co.orchestra_id, co.conductor_id)
                          for o in orchestras
                          for co in conductors_orchestras
                          if o.id == co.orchestra_id]
 
-    # 2. Итоговое соединение
-    # (Фамилия дирижера, Зарплата дирижера, Название оркестра)
     many_to_many = [(c.fio, c.salary, orch_name)
                     for orch_name, orch_id, cond_id in many_to_many_temp
                     for c in conductors if c.id == cond_id]
 
-    # --- Выполнение запросов Варианта Б ---
+    #Выполнение запросов
 
     print('Задание Б1')
     # "Выведите список всех связанных сотрудников и отделов,
@@ -95,16 +92,12 @@ def main():
     # отсортированный по количеству сотрудников."
 
     res_b2_unsorted = []
-    # Перебираем все оркестры
     for o in orchestras:
-        # Список дирижеров в текущем оркестре (из one_to_many)
         o_conds = list(filter(lambda i: i[2] == o.name, one_to_many))
 
-        # Если в оркестре есть дирижеры
         if len(o_conds) > 0:
             res_b2_unsorted.append((o.name, len(o_conds)))
 
-    # Сортировка по количеству дирижеров (по убыванию)
     res_b2 = sorted(res_b2_unsorted, key=itemgetter(1), reverse=True)
     print(res_b2)
 
@@ -113,7 +106,6 @@ def main():
     # фамилия заканчивается на «ов», и названия их отделов (Оркестров)."
     # (Используем связь многие-ко-многим)
 
-    # Используем list comprehension для фильтрации [cite: 19]
     res_b3 = [(fio, orch_name)
               for fio, _, orch_name in many_to_many
               if fio.endswith('ов')]
